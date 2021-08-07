15f25d8b05123583bb01bd9cdf29d5ef6f4f7169
Xiaogang1949 <2628832842@qq.com>
2021-08-07 15:21:38 +0800

    修改多线程文件

--- code/多线程/multidownloadXkcd.py
+++ code/多线程/multidownloadXkcd.py
@@ -5,7 +5,7 @@ import requests, os, bs4, threading
 os.makedirs('xkcd', exist_ok=True) # store comics in ./xkcd
 
 def downloadXkcd(startComic, endComic):
- for urlNumber in range(startComic, endComic):
+    for urlNumber in range(startComic, endComic):
         # Download the page.
         print('Downloading page http://xkcd.com/%s...' % (urlNumber))
         res = requests.get('http://xkcd.com/%s' % (urlNumber))
@@ -32,7 +32,7 @@ def downloadXkcd(startComic, endComic):
 
 # Create and start the Thread objects.
 downloadThreads = []                 # a list of all the Thread objects
-for i in range(0, 1400, 100):        # loops 14 times, creates 14 threads
+for i in range(0, 300, 100):        # loops 14 times, creates 14 threads
     downloadThread = threading.Thread(target=downloadXkcd, args=(i, i + 99))
     downloadThreads.append(downloadThread)
     downloadThread.start()
